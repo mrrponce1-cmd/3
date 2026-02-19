8f0016488f1e35c0574a897c66ed91f414a7ecd6
mrrponce1-cmd <mrrponce1@tip.edu.ph>
2026-02-19 14:36:30 +0800

    Update script.js
    Done

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-const form = document.getElementById("loginform");
-const loginContainer = document.getElementById("loginContainer");
-const dashboard = document.getElementById("dashboard");
-const welcome = document.getElementById("welcome");
-const toggle = document.getElementById("togglePass");
-const password = document.getElementById("password");
-const logout = document.getElementById("logout");
+const form = document.getElementById("loginForm");
 
-//hidden hide password
-toggle.onclick = () => {
-    password.type = password.type === "password" ? "text" : "password";
-};
-
-//login
-form.onsubmit = (e) =>{
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    loginContainer.classList.add("hidden");
-    dashboard.classList.remove("hidden");
-    welcome.textContent = 'Welcome back, ${email}';
-};
-
-//logout
-logout.onclick = ()=>{
-    dashboard.classList.add("hidden");
-    loginContainer.classList.remove("hidden");
-}
+form.addEventListener("submit", e=>{
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  alert("Welcome " + email);
+});
